67aff4db24d652beacfa86c413c71ce038fddf32
Thiago <tms1_tms2@hotmail.com>
2021-08-28 11:31:34 -0300

    Header component

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,18 @@
+import Image from "next/image";
+import logoImg from "../../../public/images/logo.svg";
+
+import styles from "./styles.module.scss";
+
+export function Header() {
+  return (
+    <header className={styles.headerContainer}>
+      <div className={styles.headerContent}>
+        <Image src={logoImg} alt="ig.news" />
+        <nav>
+          <a className={styles.active} href="">Home</a>
+          <a href="">Posts</a>
+        </nav>
+      </div>
+    </header>
+  );
+}
