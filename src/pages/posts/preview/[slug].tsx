f84fb98faed3210308eb05fc07bf32b6b176965c
Thiago <tms1_tms2@hotmail.com>
2021-09-14 16:21:25 -0300

    Redirect if user has subscription

--- src/pages/posts/preview/[slug].tsx
+++ src/pages/posts/preview/[slug].tsx
@@ -1,7 +1,10 @@
 import { GetStaticProps } from "next";
+import { useSession } from "next-auth/client";
 import Head from "next/head";
 import Link from "next/link";
+import { useRouter } from "next/router";
 import { RichText } from "prismic-dom";
+import { useEffect } from "react";
 import { getPrismicClient } from "../../../services/prismic";
 
 import styles from "../post.module.scss";
@@ -16,6 +19,15 @@ interface PostPreviewProps {
 }
 
 export default function PostPreview({ post }: PostPreviewProps) {
+  const [session] = useSession();
+  const router = useRouter();
+
+  useEffect(() => {
+    if (session?.activeSubscription) {
+      router.push(`/posts/${post.slug}`);
+    }
+  }, [session]);
+
   return (
     <>
       <Head>
@@ -69,6 +81,7 @@ export const getStaticProps: GetStaticProps = async ({ params }) => {
   };
 
   return {
-    props: { post, }
+    props: { post, },
+    revalidate: 60 * 30, // 30 minutes
   };
 }
